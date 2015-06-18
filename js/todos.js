c406da4a91e79383f6090b0a5d978a7aac2ac082
dodogee <binch2010@nate.com>
2015-06-18 20:41:57 +0900

    Update todos.js

--- js/todos.js
+++ js/todos.js
@@ -2,13 +2,12 @@
 // [Jérôme Gravel-Niquet](http://jgn.me/). This demo uses Parse to persist
 // the todo items and provide user authentication and sessions.
 
-$(function() {
+$(document).ready(function() {
 
   Parse.$ = jQuery;
 
   // Initialize Parse with your Parse application javascript keys
-  Parse.initialize("your-application-id",
-                   "your-javascript-key");
+  Parse.initialize("cfwULV0DDbuYUytwY7Ve9gyrFvKM2jJcGWabPqIU", "lUKeO8vWE2QxQwdAFNZhKpKOW3IhKQyd0YOMghcv");
 
   // Todo Model
   // ----------
@@ -154,7 +153,7 @@ $(function() {
       "click #clear-completed": "clearCompleted",
       "click #toggle-all": "toggleAllComplete",
       "click .log-out": "logOut",
-      "click ul#filters a": "selectFilter",
+      "click ul#filters a": "selectFilter"
     },
 
     el: ".content",
@@ -164,11 +163,10 @@ $(function() {
     // loading any preexisting todos that might be saved to Parse.
     initialize: function() {
       var self = this;
-
       _.bindAll(this, 'addOne', 'addAll', 'addSome', 'render', 'toggleAllComplete', 'logOut', 'createOnEnter');
 
       // Main todo management template
-      this.$el.html(_.template($("#manage-todos-template").html()));
+      this.$el.html(_.template($('#manage-todos-template').html()));
       
       this.input = this.$("#new-todo");
       this.allCheckbox = this.$("#toggle-all")[0];
@@ -292,51 +290,6 @@ $(function() {
     }
   });
   
-  var LogInView = Parse.View.extend({
-    events: {
-      "submit form.login-form": "logIn",
-      "click .joinLink": "SignUpView"
-    },
-
-    el: ".content",
-    
-    initialize: function() {
-      _.bindAll(this, "logIn", "SignUpView");
-      this.render();
-    },
-
-    logIn: function(e) {
-      var self = this;
-      var username = this.$("#login-email").val();
-      var password = this.$("#login-password").val();
-      
-      Parse.User.logIn(email, password, {
-        success: function(user) {
-          new ManageTodosView();
-          self.undelegateEvents();
-          delete self;
-        },
-
-        error: function(user, error) {
-          self.$(".login-form .error").html("Invalid email or password. Please try again.").show();
-          self.$(".login-form button").removeAttr("disabled");
-        }
-      });
-      
-      this.$(".login-form button").attr("disabled", "disabled");
-
-      return false;
-    },
-    SignUpView: function(e) {
-      new SignUpView();
-      this.undelegateEvents();
-      delete this;
-    },
-    render: function() {
-      this.$el.html(_.template($("#login-template").html()));
-      this.delegateEvents();
-    }
-  });
   var SignUpView = Parse.View.extend({
     events: {
       "submit form.signup-form": "signUp"
@@ -355,20 +308,27 @@ $(function() {
       var username = this.$("#signup-username").val();
       var password = this.$("#signup-password").val();
       
-      Parse.User.signUp(email, username, password, { ACL: new Parse.ACL() }, {
+      user.signUp(username, password, {
+        ACL: new Parse.ACL(), email: email }, {
         success: function(user) {
-          new ManageTodosView();
-          self.undelegateEvents();
-          delete self;
+          if (user.get("emailVerified")) {
+              new ManageTodosView();
+              self.undelegateEvents();
+              delete self;
+          } else {
+              Parse.User.logOut();
+              self.$(".login-form .error").html("메시지를 보냈습니다. 이메일을 확인해주세요!").show();
+              this.$(".login-form button").removeAttr("disabled");
+          }
         },
 
         error: function(user, error) {
-          self.$(".signup-form .error").html(_.escape(error.message)).show();
+          self.$(".signup-form .error").html("정보를 정확하게 입력해주세요!").show();
           self.$(".signup-form button").removeAttr("disabled");
         }
       });
 
-      this.$(".signup-form button").attr("disabled", "disabled", "disabled");
+      this.$(".signup-form button").attr("disabled", "disabled");
 
       return false;
     },
@@ -377,7 +337,75 @@ $(function() {
       this.$el.html(_.template($("#signup-template").html()));
       this.delegateEvents();
     }
+  });  
+  var LogInView = Parse.View.extend({
+    events: {
+      "submit form.login-form": "logIn",
+      "click .joinLink": "signUpLink",
+      "click .LoginButtonWithFacebook": "loginWithFacebook"
+    },
+
+    el: ".content",
+    
+    initialize: function() {
+      _.bindAll(this, "logIn", "signUpLink", "loginWithFacebook");
+      this.render();
+    },
+
+    logIn: function(e) {
+      var self = this;
+      var email = this.$("#login-email").val();
+      var password = this.$("#login-password").val();
+      
+      Parse.User.logIn(email, password, {
+        success: function(user) {
+          if (user.get("emailVerified")) {
+              new ManageTodosView();
+              self.undelegateEvents();
+              delete self;
+          } else {
+              Parse.User.logOut();
+              self.$(".login-form .error").html("이메일 주소가 유효하지 않습니다.").show();
+              this.$(".login-form button").removeAttr("disabled");
+          }
+        },
+
+        error: function(user, error) {
+          //self.$(".signup-form .error").html(error.message).show();
+          self.$(".login-form .error").html("잘못된 이메일이나 패스워드를 입력하셨습니다.").show();
+          self.$(".login-form button").removeAttr("disabled");
+        }
+      });
+      
+      this.$(".login-form button").attr("disabled", "disabled");
+
+      return false;
+    },
+    signUpLink: function(e) {
+      new SignUpView();
+      this.undelegateEvents();
+      delete this;
+    },
+    loginWithFacebook: function(e) {
+      Parse.FacebookUtils.logIn(null, {
+      success: function(user) {
+          if (!user.existed()) {
+            alert("User signed up and logged in through Facebook!");
+          } else {
+            alert("User logged in through Facebook!");
+          }
+        },
+        error: function(user, error) {
+          alert("User cancelled the Facebook login or did not fully authorize.");
+        }
+      });
+    },
+    render: function() {
+      this.$el.html(_.template($("#login-template").html()));
+      this.delegateEvents();
+    }
   });
+
     
   // The main view for the app
   var AppView = Parse.View.extend({
