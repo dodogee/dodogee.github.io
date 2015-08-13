b12a6cc0450aebd05bf52849374ca3366efff5d6
Park June Seong <writtic@gmail.com>
2015-08-14 02:43:10 +0900

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,44 @@
 $(document).ready(function(){
-	$('body').css('overflow','hidden');
-	var height = $(window).height();
-	$('.board').css('height',height);
-	$('#select').draggable();
-	$('#simulButton').on('click', function(){
+	$('body').css('overflow','hidden');	
+	if(1){
+		var height = $(window).height();
+		var width = $(window).width();
+		$('.board').css('height',height);
+	}
+	var selector=0;
+	var x, y;
+	$('.input-item, .output-item').draggable({
+		containment:'document', 
+		revert:true,
+		scope: "tasks",
+		start:function(event, position){
+			x=position.left;
+			contents = $(this).text();
+		},
+		stop:function(){
+			selector=scope;
+		}
+	});
+
+	$('.board').droppable({
+		hoverClass:'.board',		
+		accept: '.input-item',
+		drop:function(){
+			$('.board').append('<span class="label label-primary">'+selector+'</span>');
+		}		
+	});
+
+	$('board').droppable({
+		hoverClass:'.board',	
+		accept: '.output-item',
+		drop:function(){
+			$('.board').append('<span class="label label-warning">'+selector+'</span>');
+		}
+	});
+	
+	$('#create').on('click', function(){
 		var $btn = $(this).button('loading');
+		$('.board').append('<span class="label label-primary">'+selector+'</span>');
 		$btn.button('reset')
 	});
 });
-
-
